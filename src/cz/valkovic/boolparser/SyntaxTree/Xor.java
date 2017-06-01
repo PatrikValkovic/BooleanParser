0bb73df14b265b0aa53beeddb0659f772adab3fa
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-06-01 14:05:48 +0200

    Add Xor node

---
+++ src/cz/valkovic/boolparser/SyntaxTree/Xor.java
@@ -0,0 +1,25 @@
+package cz.valkovic.boolparser.SyntaxTree;
+
+/*
+ * Created by Patrik Valkovic
+ * 01.06.2017 14:04.
+ * Part of BooleanParser
+ */
+
+import java.util.Set;
+
+public class Xor extends Node {
+
+    private Node l;
+    private Node r;
+
+    public Xor(Node l, Node r){
+        this.l = l;
+        this.r = r;
+    }
+
+    @Override
+    public Set<String> result() {
+        return new Set<String>();
+    }
+}
